66adb1075639309f574bd1bec10871a857131574
akbar-widya <akbarwidya.dev@gmail.com>
2026-07-03 04:10:09 +0700

    3. Side effect - empty array dependency; run once on first render. Side effect is used to grab something outside react, like REST API or browser DOM--and monitor changes of its value into a React state.

--- src/side-effect/Topic3Lesson2.jsx
+++ src/side-effect/Topic3Lesson2.jsx
@@ -1,5 +1,20 @@
+import { useEffect, useState } from "react";
+
+function WindowWidth() {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function handleResize() {
+        setWidth(window.innerWidth);
+    }
+
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
+
+  return <p>WIndow width: {width}</p>
+}
+
 export default function Topic3Lesson2() {
-  return (
-    <div>Topic3Lesson2</div>
-  )
+  return <WindowWidth />;
 }
