ae3b6d896b18157768211cc53890ffd97a83fe5e
akbar-widya <akbarwidya.dev@gmail.com>
2026-07-03 01:32:43 +0700

    1. Props & component composition - giving prop a default value

--- src/props-n-component/Topic1Lesson3.jsx
+++ src/props-n-component/Topic1Lesson3.jsx
@@ -1,7 +1,12 @@
+import profile from '../assets/boy-profile.png'
+
+// give default value for prop the same way giving default value for common parameter 
+function Avatar({ src, size = 40, alt = "User avatar" }) {
+    return <img src={src} width={size} height={size} alt={alt} />
+}
+
 const Topic1Lesson3 = () => {
-  return (
-    <div>Topic1Lesson3</div>
-  )
+  return <Avatar src={profile} />
 }
 
 export default Topic1Lesson3
